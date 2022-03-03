cc87d35f6bba082da617d095b5a7942033180835
POLARIS\Sulistyo Hartanto <cuyol13@gmail.com>
2022-03-03 19:49:01 +0700

    incomplete

---
+++ lib/services/image_picker_service.dart
@@ -0,0 +1,33 @@
+import 'package:image_picker/image_picker.dart';
+
+// pretty useless class for now because the imagepicker method could be used
+// directly in the update profile screen
+class ImagePickerService {
+  XFile? image;
+  static final _imagepickerservice = ImagePickerService._();
+
+  ImagePickerService._();
+
+  factory ImagePickerService() {
+    return _imagepickerservice;
+  }
+
+  final ImagePicker _picker = ImagePicker();
+
+  Future<XFile?> retrieveLostData() async {
+    final LostDataResponse response = await _picker.retrieveLostData();
+    if (response.isEmpty) {
+      return null;
+    } else if (response.file != null) {
+      return response.file;
+    } else {
+      print(response.exception!.message);
+      return null;
+    }
+  }
+
+  Future<XFile?> retrieveImage() async {
+    image = await _picker.pickImage(source: ImageSource.gallery);
+    return image;
+  }
+}
