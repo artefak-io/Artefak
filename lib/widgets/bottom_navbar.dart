b5d1be25c740f771ea6d68e37016bbce87618dc7
12henbx <henoch_manampiring@yahoo.com>
2022-06-18 17:46:16 +0700

    Adjust bottomNav, AppBar, and color Theme to the design

--- lib/widgets/bottom_navbar.dart
+++ lib/widgets/bottom_navbar.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class BotNavBar extends StatelessWidget {
@@ -14,6 +16,12 @@ class BotNavBar extends StatelessWidget {
         Navigator.pushNamed(context, '/collection');
         break;
       case 2:
+        Navigator.pushNamed(context, '/like');
+        break;
+      case 3:
+        Navigator.pushNamed(context, '/transaction');
+        break;
+      case 4:
         Navigator.pushNamed(context, '/profile');
         break;
     }
@@ -22,29 +30,46 @@ class BotNavBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     ThemeData _themeData = Theme.of(context);
-    return SizedBox(
-      height: 80,
-      child: BottomNavigationBar(
-        type: BottomNavigationBarType.fixed,
-        backgroundColor: Colors.blue.withOpacity(0.1), //_themeData.highlightColor,
-        selectedItemColor: _themeData.textSelectionColor,
-        unselectedItemColor: _themeData.unselectedWidgetColor,
-        items: <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home_rounded),
-            label: 'Home',
+    return ClipRect(
+      child: BackdropFilter(
+        filter: ImageFilter.blur(
+          sigmaX: 5.0,
+          sigmaY: 5.0,
+        ),
+        child: SizedBox(
+          height: 80,
+          child: BottomNavigationBar(
+            elevation: 0,
+            type: BottomNavigationBarType.fixed,
+            backgroundColor: _themeData.highlightColor,
+            selectedItemColor: _themeData.textSelectionColor,
+            unselectedItemColor: _themeData.unselectedWidgetColor,
+            items: <BottomNavigationBarItem>[
+              BottomNavigationBarItem(
+                icon: Icon(Icons.home_rounded),
+                label: 'Home',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.inventory_2),
+                label: 'Koleksi',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.favorite_border_rounded),
+                label: 'Suka',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.receipt_long_rounded),
+                label: 'Transaksi',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.account_circle_outlined),
+                label: 'Akun',
+              ),
+            ],
+            currentIndex: currentIndex,
+            onTap: (index) => _onTap(index, context),
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.inventory_2),
-            label: 'Koleksi',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.account_circle_outlined),
-            label: 'Akun',
-          ),
-        ],
-        currentIndex: currentIndex,
-        onTap: (index) => _onTap(index, context),
+        ),
       ),
     );
   }
