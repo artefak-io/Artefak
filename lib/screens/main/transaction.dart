b3fbf40b2a2f720257ffc0a313d33effd93e477f
12henbx <henoch_manampiring@yahoo.com>
2022-07-02 18:27:16 +0700

    show hide transaction filter

--- lib/screens/main/transaction.dart
+++ lib/screens/main/transaction.dart
@@ -7,6 +7,8 @@ import 'package:artefak/widgets/bottom_navbar.dart';
 import 'package:artefak/widgets/radio_button_filter_item.dart';
 import 'package:artefak/widgets/transaction_row_item.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/scheduler.dart';
 
 enum TransactionFilter { all, wait, active, missed }
 
@@ -19,8 +21,16 @@ class Transaction extends StatefulWidget {
   State<Transaction> createState() => _TransactionState();
 }
 
-class _TransactionState extends State<Transaction> {
+class _TransactionState extends State<Transaction>
+    with TickerProviderStateMixin {
   List<CustomRadioModel> filterList = <CustomRadioModel>[];
+
+  final ScrollController _scrollController = ScrollController();
+  late final AnimationController _filterAnimationController = AnimationController(
+    vsync: this,
+    duration: Duration(milliseconds: 400),
+  );
+  bool visible = true;
 
   @override
   void initState() {
@@ -35,6 +45,7 @@ class _TransactionState extends State<Transaction> {
   Widget build(BuildContext context) {
     ThemeData _themeData = Theme.of(context);
     TextTheme _textTheme = Theme.of(context).textTheme;
+    Size size = MediaQuery.of(context).size;
 
     if (AuthService.user == null) {
       return const Authenticate();
@@ -57,82 +68,144 @@ class _TransactionState extends State<Transaction> {
                       sigmaY: 5.0,
                     ),
                     child: AppBar(
-                      toolbarHeight: 64.0,
-                      automaticallyImplyLeading: false,
-                      title: Text(
-                        'Transaksi',
-                        style: _textTheme.titleLarge
-                            ?.copyWith(fontWeight: FontWeight.w400),
-                      ),
-                      actions: [
-                        IconButton(
-                            icon: Icon(Icons.notifications_none, size: 25.0),
-                            onPressed: () {}),
-                      ],
-                    ),
+                          elevation: 1,
+                          toolbarHeight: 64.0,
+                          automaticallyImplyLeading: false,
+                          title: Text(
+                            'Transaksi',
+                            style: _textTheme.titleLarge
+                                ?.copyWith(fontWeight: FontWeight.w400),
+                          ),
+                          actions: [
+                            IconButton(
+                                icon: Icon(Icons.notifications_none, size: 28.0),
+                                onPressed: () {}),
+                          ],
+                        ),
                   ),
                 ),
               ],
             ),
           ),
-          body: SingleChildScrollView(
-            physics: ClampingScrollPhysics(),
+          body: Container(
+            height: size.height,
             child: Stack(
-              children: <Widget>[
-                Positioned(
-                  left: 0,
-                  right: -120,
-                  top: -85,
-                  child: Image.asset(
-                    'assets/bggrad.png',
-                    fit: BoxFit.fitHeight,
-                    height: 350,
+                children: <Widget>[
+                  Positioned(
+                    left: 0,
+                    right: -120,
+                    top: -85,
+                    child: Container(
+                      height: 400,
+                      width: 400,
+                      child: Image.asset(
+                        'assets/bggrad.png',
+                        fit: BoxFit.fitHeight,
+                        height: 350,
+                      ),
+                    ),
                   ),
-                ),
-                Column(
-                  children: [
-                    Container(
-                      padding:
-                          EdgeInsets.only(top: 8.0, bottom: 8.0, left: 16.0),
-                      margin: EdgeInsets.only(top: 64.0),
-                      height: 48.0,
-                      color: _themeData.primaryColorDark,
-                      child: ListView.builder(
-                        itemCount: filterList.length,
-                        scrollDirection: Axis.horizontal,
-                        itemBuilder: (BuildContext context, int index) {
-                          return InkWell(
-                            onTap: () {
-                              setState(() {
-                                filterList.forEach(
-                                    (element) => element.isSelected = false);
-                                filterList[index].isSelected = true;
-                              });
-                            },
-                            child: new RadioButtonFilterItem(filterList[index]),
-                          );
+                  Positioned(
+                    top: 0.0,
+                    child: Container(
+                      height: size.height,
+                      width: 400,
+                      child: NotificationListener<ScrollNotification>(
+                        onNotification: (scrollNotification) {
+                          print('inside the onNotification');
+                          if (_scrollController.hasClients) {
+                            if (_scrollController.position.userScrollDirection ==
+                                ScrollDirection.reverse) {
+                              _filterAnimationController.forward();
+                            } else if (_scrollController
+                                    .position.userScrollDirection ==
+                                ScrollDirection.forward) {
+                              _filterAnimationController.reverse();
+                            }
+                          }
+                          return true;
                         },
+                        child: SingleChildScrollView(
+                          controller: _scrollController,
+                          physics: ClampingScrollPhysics(),
+                          child: Column(
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.only(top: 128.0, left: 16.0, right: 16.0, bottom: 64.0),
+                                child: Column(
+                                  children: [
+                                    TransactionRowItem(),
+                                    SizedBox(
+                                      height: 16,
+                                    ),
+                                    TransactionRowItem(),
+                                    SizedBox(
+                                      height: 16,
+                                    ),
+                                    TransactionRowItem(),
+                                    SizedBox(
+                                      height: 16,
+                                    ),
+                                    TransactionRowItem(),
+                                    SizedBox(
+                                      height: 16,
+                                    ),
+                                    TransactionRowItem(),
+                                    SizedBox(
+                                      height: 16,
+                                    ),
+                                    TransactionRowItem(),
+                                    SizedBox(
+                                      height: 80.0,
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
                       ),
                     ),
-                    Padding(
-                      padding: const EdgeInsets.all(16.0),
-                      child: Column(
-                        children: [
-                          TransactionRowItem(),
-                          SizedBox(
-                            height: 16,
+                  ),
+                  Positioned(
+                    top: 0,
+                    child: Container(
+                      height: 110,
+                      width: 400,
+                      child: SlideTransition(
+                        position:
+                        Tween<Offset>(begin: Offset.zero, end: Offset(0, -1))
+                            .animate(
+                          CurvedAnimation(
+                              parent: _filterAnimationController, curve: Curves.fastOutSlowIn),
+                        ),
+                        child: Container(
+                          padding: EdgeInsets.only(top: 8.0, bottom: 8.0, left: 16.0),
+                          margin: EdgeInsets.only(top: 64.0),
+                          height: 48.0,
+                          color: _themeData.primaryColorDark,
+                          child: ListView.builder(
+                            itemCount: filterList.length,
+                            scrollDirection: Axis.horizontal,
+                            itemBuilder: (BuildContext context, int index) {
+                              return InkWell(
+                                onTap: () {
+                                  setState(() {
+                                    filterList.forEach(
+                                            (element) => element.isSelected = false);
+                                    filterList[index].isSelected = true;
+                                  });
+                                },
+                                child: new RadioButtonFilterItem(filterList[index]),
+                              );
+                            },
                           ),
-                          TransactionRowItem(),
-                          SizedBox(
-                            height: 80.0,
-                          ),
-                        ],
+                        ),
                       ),
                     ),
-                  ],
-                ),
-              ],
-            ),
+                  ),
+                ],
+              ),
           ),
           bottomNavigationBar: const BotNavBar(
             currentIndex: Transaction.index,
